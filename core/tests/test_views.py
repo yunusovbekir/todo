827bb7de485cf8dead2940bafeb0b658c1828aa1
Bakir Yunusov <yunusovbekir@gmail.com>
2020-06-04 02:28:37 +0400

    add test

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -4,10 +4,13 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import override_settings, SimpleTestCase
 from django.urls import reverse
 from ..models import Portfolio
+from ..forms import ContactMessageForm
 
-
-INDEX_URL = reverse('index')
 MEDIA_ROOT = tempfile.mkdtemp()
+
+
+def create_portfolio(**data):
+    return Portfolio.objects.create(**data)
 
 
 @override_settings(MEDIA_ROOT=MEDIA_ROOT)
@@ -32,6 +35,7 @@ class IndexViewTestCases(SimpleTestCase):
             'photo_detail_page': photo_detail,
             'url': 'https://google.com',
         }
+        self.index_url = reverse('index')
 
     @classmethod
     def tearDownClass(cls):
@@ -42,7 +46,7 @@ class IndexViewTestCases(SimpleTestCase):
         """
         Testing home page without saving any data to database
         """
-        response = self.client.get(path=INDEX_URL)
+        response = self.client.get(path=self.index_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/home/index.html')
 
@@ -55,14 +59,62 @@ class IndexViewTestCases(SimpleTestCase):
         for each in range(10):
             if each <= 4:
                 # 5 status = True
-                Portfolio.objects.create(**self.portfolio_data)
+                create_portfolio(**self.portfolio_data)
             else:
                 # status = False
-                Portfolio.objects.create(**self.portfolio_data, status=False)
+                create_portfolio(**self.portfolio_data, status=False)
 
         all_portfolio = Portfolio.objects.all()
-        response = self.client.get(path=INDEX_URL)
+        response = self.client.get(path=self.index_url)
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context.get('portfolio'), 5)
         self.assertTrue(all_portfolio.count(), 10)
+
+
+class ContactViewTestCases(SimpleTestCase):
+    databases = '__all__'
+
+    def test_contact_view(self):
+        response = self.client.get(reverse('contact'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/index.html')
+        self.assertIsInstance(response.context.get('form'), ContactMessageForm)
+
+
+class PortfolioDetailViewTestCases(SimpleTestCase):
+    databases = '__all__'
+
+    @override_settings(MEDIA_ROOT=MEDIA_ROOT)
+    def setUp(self):
+        photo = SimpleUploadedFile(
+            "test_photo.png",
+            b"content_in_bytes",
+            content_type='png'
+        )
+        photo_detail = SimpleUploadedFile(
+            "test_photo.png",
+            b"content_for_desc_in_bytes",
+            content_type='png'
+        )
+        self.portfolio_data = {
+            'title': 'testing_title',
+            'description': 'description testing',
+            'photo': photo,
+            'photo_detail_page': photo_detail,
+            'url': 'https://google.com',
+        }
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+        super().tearDownClass()
+
+    def test_portfolioO(self):
+        portfolio = create_portfolio(**self.portfolio_data)
+        url = reverse('portfolio', kwargs={'pk': portfolio.id})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'portfolio-details.html')
